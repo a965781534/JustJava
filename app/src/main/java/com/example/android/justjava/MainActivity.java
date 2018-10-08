b84f69201b207dfc79efc62172b95c13ccf8e314
言未尽 <a965781534@163.com>
2018-10-08 17:19:50 +0800

    change the word

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total = $"  + quantity * 5;
+        String priceMessage = "Total: $"  + quantity * 5 + "\n" + "Thank you!";
         displayMessage(priceMessage);
     }
 
